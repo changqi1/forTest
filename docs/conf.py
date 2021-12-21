a5b2265ba4fc0531c549cf29c2bb52b14b2dce33
chenbangduo <chenbangduo@B-LB3ALVDL-2124.local>
2021-12-21 16:40:38 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -1,60 +1,35 @@
-# Copyright 2021 Alibaba Group Holding Limited. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# =============================================================================
+# Configuration file for the Sphinx documentation builder.
 
-r'''Configuration file for the Sphinx documentation builder.
+# -- Project information
 
-This file only contains a selection of the most common options. For a full
-list see the documentation:
-https://www.sphinx-doc.org/en/master/usage/configuration.html
-'''
+project = 'Lumache'
+copyright = '2021, Graziella'
+author = 'Graziella'
 
-# -- Project information -----------------------------------------------------
-project = 'HybridBackend'
-author = 'Alibaba Group Holding Limited'
-copyright = '2021 Alibaba Group Holding Limited'  # pylint: disable=redefined-builtin
-release = 'latest'
+release = '0.1'
+version = '0.1.0'
 
-# -- General configuration ---------------------------------------------------
+# -- General configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
-    'myst_parser',
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autosectionlabel',
     'sphinx.ext.autosummary',
-    'sphinx.ext.extlinks',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.todo',
-    'sphinx.ext.ifconfig'
+    'sphinx.ext.intersphinx',
 ]
 
-# Add any paths that contain templates here, relative to this directory.
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+# -- Options for HTML output
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['images']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
